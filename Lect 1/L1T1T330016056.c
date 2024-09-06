b4a52e5898ff182afbbe47f43d10bb2aea266d79
TosakaUCW <tosakaucw0@gmail.com>
2024-09-06 13:49:34 +0800

    2024/09/06

---
+++ Lect 1/L1T1T330016056.c
@@ -0,0 +1,16 @@
+/*
+=============================================
+ Name: L1T1t330016056.c
+ Author: Bohan YANG
+ Version: 1.0
+ Copyright: Your copyright notice
+ Description:
+ ============================================= */
+#include <stdio.h>
+int main()
+{
+    // printf("I am a programmer\n");
+    printf("My name is Bohan YANG\n");
+    printf("My student ID is 2330016056\n");
+    return 0;
+}
---
+++ Lect 1/L1T2T330016056.c
@@ -0,0 +1,17 @@
+/*
+=============================================
+ Name: L1T2t330016056.c
+ Author: Bohan YANG
+ Version: 1.0
+ Copyright: Your copyright notice
+ Description:
+ ============================================= */
+
+// This task is to find the compiling errors
+#include <stdio.h>
+int main()
+{
+    printf("Hello ");
+    printf("World");
+    return 0;
+}
