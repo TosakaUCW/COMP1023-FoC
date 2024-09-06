66133a54796f2677781496c268871da4c270a9a9
TosakaUCW <tosakaucw0@gmail.com>
2024-09-06 15:07:07 +0800

    Lab01

---
+++ Lab01/W1T1T330016056.c
@@ -0,0 +1,16 @@
+/*
+=============================================
+ Name: W1T1t330016056.c
+ Author: Bohan YANG
+ Version: 1.0
+ Copyright: My copyright notice
+ Description: Print Hello World
+ ============================================= */
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    // Print Hello World!
+    printf("Hello, world!\n");
+    return 0;
+    // End of the Code
+}
---
+++ Lab01/W1T2T330016056.c
@@ -0,0 +1,20 @@
+/*
+=============================================
+ Name: W1T1T330016056.c
+ Author: Bohan YANG
+ Version: 1.0
+ Copyright: My copyright notice
+ Description: Print Hello World
+ ============================================= */
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("My First C Program\n");
+    
+    // My information
+    // Print My Name
+    printf("My Name is Bohan YANG\n");
+    // Print My Student ID
+    printf("My Student ID is 2330016056\n");
+    return 0;
+}
